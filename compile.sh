07c0ef821ef30a5ed6f1676e9dc501e6d687db5e
GROSST <open.mind2876@gmail.com>
2024-04-03 14:03:52 +0300

    first commit

---
+++ compile.sh
@@ -0,0 +1,1 @@
+bun build index.ts --compile --outfile=ffj
---
+++ types.ts
@@ -0,0 +1,23 @@
+interface VideoProp {
+    [key: string]: {
+        resize: { [key: string]: VideoResizeOpts };
+        id: string;
+        sound?: string;
+    };
+}
+
+interface VideoResizeOpts {
+    name: string;
+    args: string[];
+    path: string;
+}
+
+type convert = {
+    videoPath: string;
+    audioPath?: string;
+    output: string;
+    bitrate: number;
+    duration: number;
+};
+
+export type { VideoResizeOpts, VideoProp, convert };
